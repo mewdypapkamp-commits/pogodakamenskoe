2caf3ee27adaa2b10dd78c7f39232ee3d9647deb
Mewdy_Papka <mewdypapkamp@gmail.com>
2026-05-07 18:54:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+  async function updateWeather() {
+    let lat = 48.51; 
+    let lon = 34.61;
+    try {
+      const pos = await new Promise((res, rej) => {
+        navigator.geolocation.getCurrentPosition(res, rej, { timeout: 5000 });
+      });
+      lat = pos.coords.latitude;
+      lon = pos.coords.longitude;
+      console.log("Координаты получены!");
+    } catch (e) {
+      console.log("Геолокация недоступна, используем Каменское по умолчанию");
+    }
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,surface_pressure,wind_speed_10m,cloud_cover`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const c = data.current;
+      document.getElementById('t').innerText = Math.round(c.temperature_2m) + '°C';
+      document.getElementById('w').innerText = Math.round(c.wind_speed_10m) + ' км/год';
+      document.getElementById('p').innerText = Math.round(c.surface_pressure * 0.75) + ' мм.рт. ст.';
+      document.getElementById('h').innerText = c.relative_humidity_2m + '%';
+      document.getElementById('r').innerText = c.cloud_cover + '%';
+    } catch (err) {
+      console.error("Ошибка сети при запросе погоды");
+    }
+  }
+  updateWeather();
